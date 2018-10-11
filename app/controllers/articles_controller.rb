9b4fc59f875a590bee627781238ef9ba2ac1150c
Christopher Grabski <grabc59@gmail.com>
2018-10-10 20:25:14 -0700

    added flash error messages, created show template to redirect article submissions to

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,26 @@
+class ArticlesController < ApplicationController
+
+    def show
+        @article = Article.find(params[:id])
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+            flash[:notice] = "Article successfully created"
+            redirect_to article_path(@article)
+        else
+            render 'new'
+        end
+    end
+
+    private
+        def article_params
+            params.require(:article).permit(:title, :description)
+        end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
 
   get 'about', to: 'pages#about'
 
+  resources :articles
+
 end

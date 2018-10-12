c75ddbbe37a06f211825948f603162ec6270f630
Christopher Grabski <grabc59@gmail.com>
2018-10-11 19:34:23 -0700

    update articles is working

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,15 @@
 class ArticlesController < ApplicationController
 
-    def show
-        @article = Article.find(params[:id])
+    def index
+        @articles = Article.all
     end
 
     def new
         @article = Article.new
+    end
+
+    def edit
+        @article = Article.find(params[:id])
     end
 
     def create
@@ -16,6 +20,20 @@ class ArticlesController < ApplicationController
         else
             render 'new'
         end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] = "Article was successfully updated"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
+    end
+
+    def show
+        @article = Article.find(params[:id])
     end
 
     private

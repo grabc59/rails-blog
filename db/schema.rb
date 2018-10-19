ed8004c58dd823a2b4063df60d2d49143378483e
Christopher Grabski <grabc59@gmail.com>
2018-10-19 13:42:23 -0700

    created Users table and model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_12_040243) do
+ActiveRecord::Schema.define(version: 2018_10_19_203420) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2018_10_12_040243) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text "image", default: "https://picsum.photos/200/300"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end

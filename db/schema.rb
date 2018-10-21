55d5cc5995b274f79d4af43747d55f89c645f42d
Christopher Grabski <grabc59@gmail.com>
2018-10-20 17:04:28 -0700

    add user-article association and debugger

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_19_203420) do
+ActiveRecord::Schema.define(version: 2018_10_20_232639) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2018_10_19_203420) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.text "image", default: "https://picsum.photos/200/300"
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
